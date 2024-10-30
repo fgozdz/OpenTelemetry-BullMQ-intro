0d8db5e24af250be546528988161d7da90c37f92
fgozdz <fgozdz@ritenrg.com>
2024-10-30 15:35:11 +0100

    postgres-setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import config from './config';
 import router from './routes';
+import pgClient from './db';
 
 const app = express();
 
 const port = config.port || 3000;
+
+(async () => {
+  await pgClient.init();
+})();
 
 app.use(express.json());
 
@@ -12,4 +17,4 @@ app.use(router);
 
 app.listen(port, () => {
   console.log(`Listening ${port}`);
-})
+});
--- src/services/newsletter.service.ts
+++ src/services/newsletter.service.ts
@@ -1,26 +1,43 @@
 import nodemailer from '../nodemailer';
+import SubscribedUserCrud from '../crud/subscribedUser.crud';
 
 class NewsletterService {
-    constructor() {}
+    private subscribedUserCRUD: typeof SubscribedUserCrud;;
+
+    constructor() {
+        this.subscribedUserCRUD = SubscribedUserCrud;
+    }
 
     async subscribeToNewsletter(email: string) {
+        const subscribedUser = await this.subscribedUserCRUD.create(email);
+        if (!subscribedUser) {
+            return false;
+        }
+
         await nodemailer.sendMail({
             from: 'newsletter@example.email',
             subject: 'Subscribed to newsletter',
             text: 'You have succesfully subscribed to a newsletter',
             to: `${email}`,
         });
-        return true;
+
+        return subscribedUser;
     }
 
     async unsubcribeFromNewsletter(email: string) {
+        const removedUser = await this.subscribedUserCRUD.delete(email);
+        if (!removedUser) {
+            return false;
+        }
+
         await nodemailer.sendMail({
             from: 'newsletter@example.email',
             subject: 'Unsubscribed from a newsletter',
             text: 'You have succesfully unsubscribed from a newsletter',
             to: `${email}`,
         })
-        return true;
+        
+        return removedUser;
     }
 }
 
